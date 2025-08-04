e16b9615d3b02cb7d97aa64f8d13fbc9aa79028c
Agilesh S <agilesh304@gmail.com>
2025-08-04 22:57:52 +0530

    datatypes.c

---
+++ datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i = 10;
+    float f = 5.5;
+    char c = 'A';
+    double d = 123.456;
+
+    printf("int: %d\nfloat: %.2f\nchar: %c\ndouble: %.3lf\n", i, f, c, d);
+    return 0;
+}
