8f82427768819acb259a1bfe7f930c28af62e3ec
Agilesh S <agilesh304@gmail.com>
2025-08-04 22:58:22 +0530

    variables.c

---
+++ variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    float height = 5.9;
+    char grade = 'A';
+
+    printf("Age: %d\nHeight: %.1f\nGrade: %c\n", age, height, grade);
+    return 0;
+}
